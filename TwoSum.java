ded75c0079ab25ad61aa9c10f2c29ce5112e45ae
kanishthaaaa <kanishtha3122001@gmail.com>
2023-08-17 18:08:27 +0530

    Binary Search

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,10 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
 import java.util.*;
 
 public class TwoSum {
@@ -9,8 +16,9 @@ public class TwoSum {
         for(int i = 0 ; i<nums.length ; i++){
             nums[i] = sc.nextInt();
         }
-        ans =nums;
-        System.out.println(ans);
+
+        ans =twoSum(nums, target);
+        System.out.println();
     }
 
     public int[] twoSum(int[] nums, int target) {
